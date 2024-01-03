04fd60a003c23f35be43ca8c1dcd4e3f8accc27e
Jaren Glenn <jarenglenn@gmail.com>
2024-01-03 15:36:25 -0500

    hypr variable changes

--- dots/fish/conf.d/aliases.fish
+++ dots/fish/conf.d/aliases.fish
@@ -2,6 +2,7 @@
 alias la="eza -la --icons"
 alias gotop="gotop --nvidia"
 alias kitty-reload="kill -SIGUSR1 (pgrep kitty)"
+alias icat="kitty +kitten icat"
 
 # Local Keycloak Instance
 alias keycloak="docker run -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=password -p 8080:8080 jboss/keycloak:13.0.1"
