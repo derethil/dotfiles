d15c7833138eea18bcdfcd0bc92530b9e9de8676
Jaren Glenn <jarenglenn@gmail.com>
2024-01-05 16:45:18 -0500

    mkdir fn and global gitignore

--- dots/fish/conf.d/misc_small_functions.fish
+++ dots/fish/conf.d/misc_small_functions.fish
@@ -5,3 +5,16 @@ end
 function refish
     source ~/.config/fish/config.fish
 end
+
+function mkdir -d "Create a directory and set CWD"
+    command mkdir $argv
+    if test $status = 0
+        switch $argv[(count $argv)]
+            case '-*'
+
+            case '*'
+                cd $argv[(count $argv)]
+                return
+        end
+    end
+end
