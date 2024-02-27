26ab9fe640cbf03112dd06c7d658a78f600f729d
Jaren Glenn <jarenglenn@gmail.com>
2024-02-27 14:12:08 -0500

    fix(ags): HoverRevealer not closing sometimes

--- dots/ags/widgets/HoverRevealer.ts
+++ dots/ags/widgets/HoverRevealer.ts
@@ -17,38 +17,32 @@ export function HoverRevealer({
   setupRevealer,
   ...rest
 }: HoverRevealProps) {
-  let open = false;
   const vertical = direction === "down" || direction === "up";
   const posStart = direction === "down" || direction === "right";
   const posEnd = direction === "up" || direction === "left";
 
   const revealer = Widget.Revealer({
-    // @ts-ignore
     transition: `slide_${direction}`,
     setup: setupRevealer,
     transition_duration: duration,
     child,
   });
 
+  const handleHoverEvent = (hover: boolean) => {
+    if (hover !== revealer.reveal_child) revealer.reveal_child = hover;
+  };
+
   const eventbox = Widget.EventBox({
     ...rest,
-    setup: setupEventBox,
-    on_hover: () => {
-      if (open) return;
-
-      revealer.reveal_child = true;
-      Utils.timeout(duration, () => (open = true));
-    },
-    on_hover_lost: () => {
-      if (!open) return;
-
-      revealer.reveal_child = false;
-      open = false;
-    },
     child: Widget.Box({
       vertical,
       children: [posStart && indicator, revealer, posEnd && indicator],
     }),
+    setup: (eb) => {
+      eb.on("enter-notify-event", () => handleHoverEvent(true));
+      eb.on("leave-notify-event", () => handleHoverEvent(false));
+      setupEventBox?.(eb);
+    },
   });
 
   return eventbox;
