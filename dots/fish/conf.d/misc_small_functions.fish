30b5cb9e54f66f4a0027952f5f1076e61a4ee560
Jaren Glenn <jarenglenn@gmail.com>
2024-03-01 23:22:55 -0500

    feat(fish): fixed git clone cd, added git switch abbr

--- dots/fish/conf.d/misc_small_functions.fish
+++ dots/fish/conf.d/misc_small_functions.fish
@@ -19,14 +19,23 @@ function mkdir -d "Create a directory and set CWD"
     end
 end
 
-# Always cd after git clone
 function git
-    command git $argv
-    if test $status = 0
-        switch $argv[1]
-            case 'clone'
-                cd $argv[(count $argv)]
-                return
+    if test $argv[1] = "clone"
+        # Store the output of the original git clone command
+        set -l output (command git $argv)
+
+        # Extract the directory name from the clone URL
+        set -l dir (basename -s .git $argv[-1])
+
+        # Change directory into the cloned directory if the clone was successful
+        if test $status -eq 0
+            cd $dir
         end
+
+        # Return the output of the original git clone command
+        echo $output
+    else
+        # For any other git command, execute it as usual
+        command git $argv
     end
 end
