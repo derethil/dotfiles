4f3b45cba6218588bbc37d050d55c3091eacf1e1
Jaren Glenn <jarenglenn@gmail.com>
2024-04-23 13:23:13 -0400

    feat(ags): clock icon corresponds with hour

--- dots/ags/lib/icons.ts
+++ dots/ags/lib/icons.ts
@@ -23,10 +23,6 @@ export const icons = {
     screenshot: "",
   },
 
-  clock: "",
-
-  tray: "󱊗",
-
   mediaControls: "",
 
   audio: {
--- dots/ags/windows/bar/modules/Clock.ts
+++ dots/ags/windows/bar/modules/Clock.ts
@@ -1,14 +1,27 @@
-import { icons } from "lib/icons";
+import GLib from "gi://GLib?version=2.0";
 import { Clock as ClockWidget } from "widgets/Clock";
-import { FontIcon } from "widgets/FontIcon";
 import { IconModule } from "../IconModule";
+
+function selectClockIcon(time: GLib.DateTime): string {
+  const hours = (time.get_hour() % 12) + 1;
+  const returnValue = `time-${hours}`;
+  console.log(returnValue);
+  return returnValue;
+}
+
+const DateTime = Variable(GLib.DateTime.new_now_local(), {
+  poll: [1000 * 300, () => {
+    return GLib.DateTime.new_now_local();
+  }],
+});
 
 export function Clock() {
   return IconModule({
-    className: "clock",
+    className: " clock",
     child: ClockWidget({ format: `%I\n%M` }),
-    icon: FontIcon({
-      label: icons.clock,
+    icon: Widget.Icon({
+      icon: DateTime.bind("value").as((time) => selectClockIcon(time)),
+      size: 24,
     }),
     labelColor: "yellow",
   });
