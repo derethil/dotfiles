82c8ba3ff6f2760c8c30c36146b53442b0dd2050
Jaren Glenn <jarenglenn@gmail.com>
2024-05-10 12:02:20 -0400

    feat(neovim): add git-conflicts.nvim for conflict resolution

--- dots/nvim/lua/plugins/editor.lua
+++ dots/nvim/lua/plugins/editor.lua
@@ -8,4 +8,25 @@ return {
   {
     "tikhomirov/vim-glsl",
   },
+  {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    lazy = false,
+    config = function(_, opts)
+      require("git-conflict").setup(opts)
+    end,
+    opts = {
+      default_mappings = false,
+      default_commands = true,
+    },
+    keys = {
+      { "<leader>mo", "<cmd>GitConflictCHooseOurs<CR>", "Git Conflict: Choose Ours" },
+      { "<leader>mt", "<cmd>GitConflictChooseTheirs<CR>", "Git Conflict: Choose Theirs" },
+      { "<leader>mb", "<cmd>GitConflictChooseBoth<CR>", "Git Conflict: Choose Both" },
+      { "<leader>m0", "<cmd>GitConflictChooseNone<CR>", "Git Conflict: Choose None" },
+      { "[m", "<cmd>GitConflictPrevConflict<CR>", "Go to Previous Conflict" },
+      { "]m", "<cmd>GitConflictNextConflict<CR>", "Go to Next Conflict" },
+      { "<leader>xm", "<cmd>GitConflictListQf<CR>", "Git Conflicts List" },
+    },
+  },
 }
