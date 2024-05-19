762d02bfc4bb534f08a60b6b2e9a11a3b97cc4e1
Jaren Glenn <jarenglenn@gmail.com>
2024-05-19 15:55:49 -0400

    feat: added and configured tmux

---
+++ dots/nvim/lua/plugins/tmux.lua
@@ -0,0 +1,10 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  lazy = false,
+  keys = {
+    "<C-h>", "<cmd>TmuxNavigateLeft<CR>", "Tmux Navigate Left",
+    "<C-l>", "<cmd>TmuxNavigateRight<CR>", "Tmux Navigate Right",
+    "<C-j>", "<cmd>TmuxNavigateDown<CR>", "Tmux Navigate Down",
+    "<C-k>", "<cmd>TmuxNavigateUp<CR>", "Tmux Navigate Up",
+  }
+}
