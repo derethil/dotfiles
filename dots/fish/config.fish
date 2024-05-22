475d184f95e9d6b915a0e108ff1e4b6f27716515
Jaren Glenn <jarenglenn@gmail.com>
2024-05-22 11:06:01 -0400

    feat(shell): move to pyenv since asdf-python is broken

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -1,9 +1,14 @@
+fish_add_path $PYENV_ROOT/bin
+
 # Sources
 source $ASDF_DATA_DIR/asdf.fish
 direnv hook fish | source
 starship init fish | source
 thefuck --alias | source
 zoxide init fish | source
+pyenv init - | source
+
+
 
 # Load Sudo Alias
 fish_load_sudo_alias
