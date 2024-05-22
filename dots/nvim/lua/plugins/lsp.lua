75a08800574b4a3f4499dbd7a0b7a691603b7984
Jaren Glenn <jarenglenn@gmail.com>
2024-05-22 12:51:44 -0400

    fix(neovim): disable inlay hints

--- dots/nvim/lua/plugins/lsp.lua
+++ dots/nvim/lua/plugins/lsp.lua
@@ -11,17 +11,8 @@ return {
 
   {
     "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
     opts = {
-      ---@class lspconfig.options
-      -- servers = {
-      --   glsl_analyzer = {},
-      -- },
-      -- setup = {
-      --   glsl_analyzer = function()
-      --     require("lspconfig.glsl_analyzer").setup()
-      --   end,
-      -- },
+      inlay_hints = { enabled = false }
     },
   },
 }
