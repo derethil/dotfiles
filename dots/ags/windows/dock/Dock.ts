776f2cc26509bf545b943b385d54084fef2bd8b2
Jaren Glenn <jarenglenn@gmail.com>
2024-05-27 15:59:16 -0400

    fix(ags): don't warp cursor on focus

--- dots/ags/windows/dock/Dock.ts
+++ dots/ags/windows/dock/Dock.ts
@@ -8,7 +8,9 @@ import { Taskbar } from "./Taskbar";
 const Hyprland = await Service.import("hyprland");
 
 export const focusClient = ({ address }: Client) => {
-  Hyprland.messageAsync(`dispatch focuswindow address:${address}`);
+  Hyprland.messageAsync(
+    `[[BATCH]]/keyword general:no_cursor_warps true; dispatch focuswindow address:${address}; keyword general:no_cursor_warps false`,
+  );
 };
 
 export const killClient = (client: Client) => bash(`kill -9 ${client.pid}`);
