fc5ff150b7097eb8df328e2567ed172a822d0b2b
Jaren Glenn <jarenglenn@gmail.com>
2024-05-30 10:33:12 -0400

    feat(fish): fish default shell and transfer bashrc to its config

---
+++ dots/fish/conf.d/environment.fish
@@ -0,0 +1,25 @@
+
+set -Ux XDG_CONFIG_HOME $HOME/.config
+set -Ux XDG_CACHE_HOME $HOME/.cache
+set -Ux XDG_DATA_HOME $HOME/.local/share
+set -Ux XDG_STATE_HOME $HOME/.local/state
+set -Ux XDG_RUNTIME_DIR /run/user/1000
+
+set -Ux ASDF_CONFIG_FILE $XDG_CONFIG_HOME/asdf/asdfrc
+set -Ux ASDF_DATA_DIR $XDG_DATA_HOME/asdf
+set -Ux BUN_INSTALL $XDG_DATA_HOME/bun
+set -Ux CARGO_HOME $XDG_DATA_HOME/cargo
+set -Ux DOCKER_CONFIG "$XDG_CONFIG_HOME"/docker
+set -Ux GRADLE_USER $XDG_DATA_HOME/gradle
+set -Ux __JAVA_OPTIONS "-Djava.util.prefs.userRoot $XDG_CONFIG_HOME/java"
+set -Ux NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
+set -Ux PASSWORD_STORE_DIR "$XDG_DATA_HOME"/pass
+set -Ux RUSTUP_HOME $XDG_DATA_HOME/rustup
+set -Ux PYTHON_HISTORY $XDG_DATA_HOME/python/history
+set -Ux PYENV_ROOT $XDG_DATA_HOME/pyenv
+set -Ux RYE_HOME $XDG_DATA_HOME/rye
+set -Ux GOPATH $XDG_DATA_HOME/go
+set -Ux GOMODCACHE $XDG_CACHE_HOME/go/mod
+set -Ux NUGET_PACKAGES "$XDG_CACHE_HOME"/NuGetPackages
+set -Ux HISTFILE $XDG_DATA_HOME/bash/history
+set -Ux GTK2_RC_FILES $XDG_CONFIG_HOME/gtk-2.0/gtkrc
--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -1,15 +1,21 @@
-fish_add_path $PYENV_ROOT/bin
-
-# Sources
+# Tool Sources
 source $ASDF_DATA_DIR/asdf.fish
 direnv hook fish | source
 starship init fish | source
 thefuck --alias | source
 zoxide init fish | source
-pyenv init - | source
+# pyenv init - | source
 
-# Load Sudo Alias
+# Sudo Aliases
 fish_load_sudo_alias
+
+# Modify PATH
+fish_add_path ~/.local/bin
+fish_add_path $CARGO_HOME/bin
+fish_add_path $RYE_HOME/bin
+fish_add_path $RYE_HOME/shims
+fish_add_path $GOPATH/bin
+fish_add_path $PYENV_ROOT/bin
 
 # Start Tmux
 if status is-interactive; and not set -q TMUX
