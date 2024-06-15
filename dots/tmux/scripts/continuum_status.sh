6bc1e2211130fc22531ea4f4e02286e81e5db19d
Jaren Glenn <jarenglenn@gmail.com>
2024-06-15 11:10:13 -0400

    feat(tmux): refactor continuum status

--- dots/tmux/scripts/continuum_status.sh
+++ dots/tmux/scripts/continuum_status.sh
@@ -13,29 +13,49 @@ source "$CURRENT_DIR/helpers.sh"
 source "$CURRENT_DIR/variables.sh"
 source "$CURRENT_DIR/shared.sh"
 
-get_interval() {
-	get_tmux_option "$auto_save_interval_option" "$auto_save_interval_default"
+seconds_remaining() {
+	local last_saved_timestamp
+	local interval_minutes
+	local now
+
+	last_saved_timestamp="$(get_tmux_option "$last_auto_save_option" "0")"
+	interval_minutes="$(get_tmux_option "$auto_save_interval_option" "$auto_save_interval_default")"
+	now="$(date +%s)"
+
+	local interval_seconds="$((interval_minutes * 60))"
+	local next_run="$((last_saved_timestamp + interval_seconds))"
+	local remaining="$((next_run - now))"
+
+	echo "$remaining"
 }
 
-current_timestamp() {
-	echo "$(date +%s)"
-	"$(date +%s)"
+format_status() {
+	remaining=$1
+
+	local minutes=$((remaining % 3600 / 60))
+	local seconds=$((remaining % 60))
+
+	((seconds >= 30)) && ((minutes++))
+
+	if [ "$minutes" -eq 0 ]; then
+		formatted="<1m"
+	else
+		formatted="${minutes}m"
+	fi
+
+	echo "$formatted"
 }
 
-print_status() {
-	local save_int="$(get_tmux_option "$auto_save_interval_option")"
-	local status=""
-	local style_wrap="$(get_tmux_option "$status_on_style_wrap_option" "")"
+get_status() {
+	local save_int
+	local status
 	local style_wrap
-	if [ $save_int -gt 0 ]; then
-		local last_saved_timestamp="$(get_tmux_option "$last_auto_save_option" "0")"
-		local interval_minutes="$(get_interval)"
-		local interval_seconds="$((interval_minutes * 60))"
-		local next_run="$((last_saved_timestamp + $interval_seconds))"
-		local now="$(date +%s)"
-		local secs="$((next_run - now))"
-		local til_next_save=$(printf '%d:%02d\n' $((secs % 3600 / 60)) $((secs % 60)))
-		status="$til_next_save"
+
+	style_wrap="$(get_tmux_option "$status_on_style_wrap_option" "")"
+	save_int="$(get_tmux_option "$auto_save_interval_option")"
+
+	if [ "$save_int" -gt 0 ]; then
+		status="$(format_status "$(seconds_remaining)")"
 	else
 		style_wrap="$(get_tmux_option "$status_off_style_wrap_option" "")"
 		status="off"
@@ -44,6 +64,8 @@ print_status() {
 	if [ -n "$style_wrap" ]; then
 		status="${style_wrap/$status_wrap_string/$status}"
 	fi
+
 	echo "$status"
 }
-print_status
+
+get_status
