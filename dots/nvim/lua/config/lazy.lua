87db3c04dfa23da4e4f937eda9df40dcdc36ac26
Jaren Glenn <jarenglenn@gmail.com>
2024-07-03 20:02:11 -0400

    feat(neovim): add svelte lang

--- dots/nvim/lua/config/lazy.lua
+++ dots/nvim/lua/config/lazy.lua
@@ -42,6 +42,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     -- Linting Extras
     { import = "lazyvim.plugins.extras.linting.eslint" },
