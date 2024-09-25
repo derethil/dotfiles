25d2b3cdfa79fa1915193f29766ae270216d4616
Jaren Glenn <jarenglenn@gmail.com>
2024-09-25 10:42:44 -0400

    refactor(neovim): move usercmds into their own file

--- dots/nvim/lua/config/autocmds.lua
+++ dots/nvim/lua/config/autocmds.lua
@@ -1,6 +1,6 @@
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Run User Cmds file
+require("config.usercmds")
 
--- Autocommands
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   desc = "Set filetype for git config in ~/.dotfiles/dots/git/config",
   pattern = vim.fn.expand("~/.dotfiles/dots/git/config"),
@@ -8,46 +8,3 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     vim.bo.filetype = "gitconfig"
   end,
 })
-
--- User Commands
--- Copy text to clipboard using codeblock format ```{ft}{content}```
-vim.api.nvim_create_user_command("CopyCodeBlock", function(opts)
-  local lines = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, true)
-  local content = table.concat(lines, "\n")
-  local result = string.format("```%s\n%s\n```", vim.bo.filetype, content)
-  vim.fn.setreg("+", result)
-  vim.notify("Text copied to clipboard")
-end, { range = true })
-
-vim.api.nvim_create_user_command("ConventionalCommits", function()
-  local url = "https://kapeli.com/cheat_sheets/Conventional_Commits.docset/Contents/Resources/Documents/index"
-  vim.fn.system(string.format("open %s", url))
-end, {})
-
-vim.api.nvim_create_user_command("OverseerRestartLast", function()
-  local overseer = require("overseer")
-  local tasks = overseer.list_tasks({ recent_first = true })
-  if vim.tbl_isempty(tasks) then
-    vim.notify("No tasks found", vim.log.levels.WARN)
-  else
-    overseer.run_action(tasks[1], "restart")
-  end
-end, {})
-
-vim.api.nvim_create_user_command("CloseOtherBuffers", function()
-  local windownrs = vim.api.nvim_list_wins()
-  local currbuf = vim.api.nvim_get_current_buf()
-
-  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    local is_open = false
-    for _, win in ipairs(windownrs) do
-      if vim.api.nvim_win_get_buf(win) == buf or currbuf == buf then
-        is_open = true
-        break
-      end
-    end
-    if not is_open then
-      vim.api.nvim_buf_delete(buf, { force = true })
-    end
-  end
-end, {})
