a5fcf6605ab64875dc628b26143df0f16c425f27
Jaren Glenn <jarenglenn@gmail.com>
2024-10-07 08:56:41 -0400

    feat(fish): warp directory function

---
+++ dots/fish/functions/wd.fish
@@ -0,0 +1,24 @@
+
+function wd
+    if test -z $argv
+        echo "Usage: wd <pattern>"
+        return 1
+    end
+
+    set pattern (echo "\\$argv\$")
+    set results (locate -r $pattern)
+
+    if test (count $results) -eq 0
+        echo "No results found for $argv"
+        return 1
+    end
+
+    set selected (echo $results | string split " " | fzf)
+
+    if test (count $selected) -ne 1
+        echo "No selection made"
+        return 1
+    end
+
+    z $selected
+end
