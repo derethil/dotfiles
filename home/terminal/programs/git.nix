b162a2d015b0c525d3f1b2d5ba42667f95899600
Jaren Glenn <jarenglenn@gmail.com>
2024-10-21 10:28:04 -0400

    feat: more git configuration

--- home/terminal/programs/git.nix
+++ home/terminal/programs/git.nix
@@ -1,7 +1,36 @@
-{
+{ config, ... }: {
   programs.git = {
     enable = true;
     userName = "Jaren Glenn";
     userEmail = "jarenglenn@gmail.com";
+
+    diff-so-fancy = {
+      enable = true;
+    };
+
+    core.editor = "nvim";
+
+    push.autoSetupremote = true;
+    merge.ff = false;
+    pull.ff = "only";
+
+    ignores = [
+      ".venv"
+      ".tool-versions"
+      ".envrc"
+      ".local/"
+      ".nvim.lua"
+      ".vscode/"
+      ".lazy.lua"
+      "**/*.local"
+      "*.local.*"
+      ".direnv/"
+      ".python-version"
+    ];
+
+    signing = {
+      key = "${config.home.homeDirectory}/.ssh/id_ed25519";
+      signByDefault = true;
+    };
   };
 }
