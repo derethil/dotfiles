ca67b268fcf7d988a6224d82d93f95ee24043daf
Jaren Glenn <jarenglenn@gmail.com>
2024-10-31 10:32:10 -0400

    fix(neovim): remove manual delete of old bundles

--- dots/nvim/lua/plugins/overseer.lua
+++ dots/nvim/lua/plugins/overseer.lua
@@ -3,6 +3,8 @@ local function get_cwd_as_name()
   return dir:gsub("[^A-Za-z0-9]", "_")
 end
 
+local status
+
 vim.api.nvim_create_autocmd("User", {
   desc = "Save Overseer tasks during Persistence session save",
   pattern = "PersistenceSavePre",
@@ -18,10 +20,10 @@ vim.api.nvim_create_autocmd("User", {
   callback = function()
     local overseer = require("overseer")
     overseer.load_task_bundle(get_cwd_as_name(), { ignore_missing = true })
-    local status = { overseer.STATUS.RUNNING, overseer.STATUS.PENDING }
+
+    -- List loaded tasks to notify user
     local tasks = overseer.list_tasks({ status })
     local message = "Restored " .. #tasks .. " Overseer task" .. (#tasks == 1 and "" or "s")
-
     if #tasks > 0 then
       vim.notify(message, vim.log.levels.INFO, { title = "Overseer" })
     end
@@ -30,27 +32,31 @@ vim.api.nvim_create_autocmd("User", {
 
 vim.keymap.set({ "n", "x", "v" }, "<space>or", function()
   local overseer = require("overseer")
-
   vim.cmd("OverseerRestartLast")
 
-  local status = { overseer.STATUS.RUNNING, overseer.STATUS.PENDING }
+  -- Don't do anything if there are no tasks
   local most_recent = overseer.list_tasks({ status = status, recent_fist = true })[1]
-
   if not most_recent then
     return
   end
 
-  local task_name = most_recent.name
-
-  vim.notify("Restarted last Overseer task: " .. task_name, vim.log.levels.INFO, { title = "Overseer" })
+  vim.notify("Restarted last Overseer task: " .. most_recent.name, vim.log.levels.INFO, { title = "Overseer" })
 end, { desc = "Restart Last Task" })
 
 return {
   "stevearc/overseer.nvim",
-  opts = {
-    templates = { "builtin", "user.go" },
-    bundles = {
-      autostart_on_load = true,
-    },
-  },
+  opts = function()
+    local overseer = require("overseer")
+    status = { overseer.STATUS.RUNNING, overseer.STATUS.PENDING }
+    return {
+      templates = { "builtin", "user.go", "user.nix" },
+      bundles = {
+        save_task_opts = {
+          on_conflict = "overwrite",
+          status = status,
+        },
+        autostart_on_load = true,
+      },
+    }
+  end,
 }
