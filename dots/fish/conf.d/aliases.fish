9a06bf00d38d57e7cbcc8a68409fa6f9d2fcab27
Jaren Glenn <jarenglenn@gmail.com>
2024-11-16 15:02:54 -0500

    style: agsv2 dots folder renamed to astal

--- dots/fish/conf.d/aliases.fish
+++ dots/fish/conf.d/aliases.fish
@@ -8,7 +8,7 @@ alias nv="nvim"
 
 alias udb="sudo updatedb"
 
-alias agsv2="ags run --directory ~/.config/agsv2"
+alias agsv2="ags run --directory ~/.config/astal"
 
 function rm
     command rm $argv
