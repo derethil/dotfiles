893d9bdef50bc2635942c5163cfc87848eb3b116
Jaren Glenn <jarenglenn@gmail.com>
2024-12-17 01:00:15 -0500

    feat(neovim): update to lazyvim v14 (mostly)

---
+++ dots/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "piersolenski/telescope-import.nvim",
+    dependencies = "nvim-telescope/telescope.nvim",
+  },
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      { "<leader>fb", "<cmd>FzfLua builtin<cr>", desc = "Find Telescope builtins" },
+      { "<leader>fy", "<cmd>FzfLua filetypes<cr>", desc = "Find Filestypes" },
+    },
+    opts = {
+      defaults = {
+        previewer = false,
+        preview_pager = "diff-so-fancy",
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    lazy = true,
+    keys = {
+      {
+        "<leader>fi",
+        "<cmd>Telescope import<cr>",
+        desc = "Add Import",
+      },
+    },
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      require("telescope").load_extension("import")
+    end,
+    opts = {
+      extensions = { import = { insert_at_top = true } },
+      defaults = {
+        layout_strategy = "flex",
+        sorting_strategy = "ascending",
+        layout_config = {
+          vertical = { prompt_position = "top", mirror = true },
+          horizontal = { prompt_position = "top", preview_width = 0.55 },
+          flex = { flip_columns = 150 },
+        },
+      },
+    },
+  },
+}
