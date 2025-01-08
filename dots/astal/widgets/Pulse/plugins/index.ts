54d0c5c5abdecf73b3592d1ef2b62ed16d9e43b4
Jaren Glenn <jarenglenn@gmail.com>
2025-01-07 23:18:33 -0500

    fix(tmux): fix switching out of neovim slow

--- dots/astal/widgets/Pulse/plugins/index.ts
+++ dots/astal/widgets/Pulse/plugins/index.ts
@@ -1,14 +1,12 @@
 import { Applications } from "./applications";
 import { PulseAutocomplete } from "./autocomplete";
 import { Calculate } from "./calculator";
-import { Clipboard } from "./clipboard";
 import { PowerMenu } from "./powermenu";
 import { Shell } from "./shell";
 
 export const PulsePlugins = [
   Applications,
   Calculate,
-  Clipboard,
   PowerMenu,
   PulseAutocomplete,
   Shell,
