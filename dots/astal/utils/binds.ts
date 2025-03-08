69f1e3a85c2ef9674ae5de0baf040980af07d6b2
Jaren Glenn <jarenglenn@gmail.com>
2025-03-08 17:02:17 -0500

    feat(astal): click handler

--- dots/astal/utils/binds.ts
+++ dots/astal/utils/binds.ts
@@ -1,12 +1,18 @@
-import { Gdk, Gtk } from "astal/gtk3";
+import { Astal, Gdk, Gtk } from "astal/gtk3";
 
-interface Bind {
+type Event = Gdk.Event | Astal.ClickEvent;
+
+interface Bind<T extends Event> {
   key: number;
   mod?: Gdk.ModifierType | Gdk.ModifierType[];
-  action: (widget: Gtk.Widget, event: Gdk.Event) => unknown;
+  action: (widget: Gtk.Widget, event: T) => unknown;
 }
 
-function isEventBound(bind: Bind, eventKey: number, eventMod: number) {
+function isEventBound<T extends Event>(
+  bind: Bind<T>,
+  eventKey: number,
+  eventMod: number,
+) {
   if (bind.key !== eventKey) return false;
   if (bind.mod) {
     if (!Array.isArray(bind.mod) && !(bind.mod & eventMod)) return false;
@@ -15,11 +21,20 @@ function isEventBound(bind: Bind, eventKey: number, eventMod: number) {
   return true;
 }
 
-export function createKeyHandler(...binds: Bind[]) {
+export function createKeyHandler(...binds: Bind<Gdk.Event>[]) {
   return (widget: Gtk.Widget, event: Gdk.Event) => {
     const eventKey = event.get_keyval()[1];
     const eventMod = event.get_state()[1];
     const boundTo = binds.find((b) => isEventBound(b, eventKey, eventMod));
+    if (boundTo) boundTo.action(widget, event);
+  };
+}
+
+export function createClickHandler(...binds: Bind<Astal.ClickEvent>[]) {
+  return (widget: Gtk.Widget, event: Astal.ClickEvent) => {
+    const button = event.button;
+    const mod = event.modifier;
+    const boundTo = binds.find((b) => isEventBound(b, button, mod));
     if (boundTo) boundTo.action(widget, event);
   };
 }
