dcf1d0499a500221e0dd941231792dd361bbf0e6
Jaren Glenn <jarenglenn@gmail.com>
2025-03-09 19:09:09 -0400

    refactor(astal): pull out indicator component

--- dots/astal/widgets/Dock/components/AppButton.tsx
+++ dots/astal/widgets/Dock/components/AppButton.tsx
@@ -1,4 +1,4 @@
-import { Variable } from "astal";
+import { Binding, Variable } from "astal";
 import { Gtk, Widget } from "astal/gtk3";
 import { ButtonProps } from "astal/gtk3/widget";
 import AstalHyprland from "gi://AstalHyprland";
@@ -6,13 +6,13 @@ import { options } from "options";
 import { attach } from "utils";
 import { matchClient } from "../util/matchClient";
 
-interface Props extends ButtonProps {
+interface AppButtonProps extends ButtonProps {
   icon: string;
   term: string;
   pinned?: boolean;
 }
 
-export function AppButton(props: Props) {
+export function AppButton(props: AppButtonProps) {
   const { icon, term, pinned = false, ...rest } = props;
 
   const indicatorVisible = Variable(false);
@@ -26,42 +26,48 @@ export function AppButton(props: Props) {
     self.set_tooltip_text(text);
   };
 
-  return (
-    <button
-      {...rest}
-      className="dock-button"
-      cursor="pointer"
-      setup={(self) => {
-        const conns = [
-          hypr.connect("client-added", () => onClientUpdate(self)),
-          hypr.connect("client-removed", () => onClientUpdate(self)),
-          hypr.connect("notify::focused-client", () => onClientUpdate(self)),
-        ];
+  const handleSetup = (self: Widget.Button) => {
+    const conns = [
+      hypr.connect("client-added", () => onClientUpdate(self)),
+      hypr.connect("client-removed", () => onClientUpdate(self)),
+      hypr.connect("notify::focused-client", () => onClientUpdate(self)),
+    ];
 
-        onClientUpdate(self); // Initial indicator visibility
-        self.connect("destroy", () => conns.forEach((c) => hypr.disconnect(c)));
-        rest.setup?.(self);
-      }}
-    >
+    onClientUpdate(self); // Initial indicator visibility
+    self.connect("destroy", () => conns.forEach((c) => hypr.disconnect(c)));
+    rest.setup?.(self);
+  };
+
+  return (
+    <button {...rest} className="dock-button" cursor="pointer" setup={handleSetup}>
       <overlay
         passThrough
-        overlay={
-          pinned ? (
-            <box valign={Gtk.Align.END} halign={Gtk.Align.CENTER}>
-              <box
-                className="indicator"
-                setup={(self) =>
-                  attach(indicatorVisible(), (visible) =>
-                    self.toggleClassName("shown", visible),
-                  )
-                }
-              />
-            </box>
-          ) : undefined
-        }
+        overlay={<Indicator visible={indicatorVisible()} pinned={pinned} />}
       >
         <icon icon={icon} css={options.dock.size().as((px) => `font-size: ${px}px`)} />
       </overlay>
     </button>
+  );
+}
+
+interface IndicatorProps {
+  visible: Binding<boolean>;
+  pinned?: boolean;
+}
+
+function Indicator(props: IndicatorProps) {
+  const { visible, pinned } = props;
+
+  if (!pinned) return <></>;
+
+  return (
+    <box valign={Gtk.Align.END} halign={Gtk.Align.CENTER}>
+      <box
+        className="indicator"
+        setup={(self) =>
+          attach(visible, (visible) => self.toggleClassName("shown", visible))
+        }
+      />
+    </box>
   );
 }
