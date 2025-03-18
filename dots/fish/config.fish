08dbae23eba404f9e8e935ee7316abef59c8be5b
Jaren Glenn <jarenglenn@gmail.com>
2025-03-18 01:39:53 -0400

    feat(fish): install babelfish

--- dots/fish/config.fish
+++ dots/fish/config.fish
@@ -1,11 +1,19 @@
+# Modify PATH
+fish_add_path ~/.local/bin
+fish_add_path $CARGO_HOME/bin
+fish_add_path $GOPATH/bin
+fish_add_path $PNPM_HOME
+fish_add_path (ruby -e 'puts Gem.user_dir')/bin
+
 # Tool Sources
 source $ASDF_DATA_DIR/asdf.fish
 direnv hook fish | source
-starship init fish | source
 thefuck --alias | source
+starship init fish | source
 zoxide init fish | source
 uv generate-shell-completion fish | source
 uvx --generate-shell-completion fish | source
+source "$XDG_CONFIG_HOME/fish/plugins/babel.fish"
 
 # Sudo Aliases
 fish_load_sudo_alias
