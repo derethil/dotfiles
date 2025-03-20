502889a42dd75c88e27a2ee13f09b4597cb836c1
Jaren Glenn <jarenglenn@gmail.com>
2025-03-19 23:20:41 -0400

    feat(astal): fix wlsunset for nix management

--- dots/astal/lib/wlsunset.ts
+++ dots/astal/lib/wlsunset.ts
@@ -1,14 +1,10 @@
-import { bind, property, register, Variable, GObject } from "astal";
-import { Location } from "state/location";
-import { bash, dependencies } from "utils";
+import { bind, property, register, GObject, exec } from "astal";
 
 @register({ GTypeName: "WaylandSunset" })
 export class WLSunset extends GObject.Object {
   static instance: WLSunset;
 
   static get_default() {
-    if (!dependencies("wlsunset")) throw new Error("wlsunset not found on PATH");
-
     if (!this.instance) this.instance = new WLSunset();
     return this.instance;
   }
@@ -20,39 +16,23 @@ export class WLSunset extends GObject.Object {
   @property(Boolean)
   declare enabled: boolean;
 
-  @property(Number)
-  declare lowTemperature: number;
-
-  @property(Number)
-  declare highTemperature: number;
-
   constructor() {
     // @ts-expect-error not typed correctly
-    super({ enabled: true, lowTemperature: 2800, highTemperature: 6500 });
-
-    let first = true;
-
-    Variable.derive(
-      [
-        bind(this, "enabled"),
-        bind(this, "lowTemperature"),
-        bind(this, "highTemperature"),
-      ],
-      async (enabled, low, high) => {
-        if (!first) await this.killProcess();
-        if (enabled) this.startProcess(low, high);
-        first = false;
-      },
-    );
+    super({ enabled: WLSunset.active });
+    this.syncServiceStatus();
   }
 
-  async killProcess() {
-    await bash(`killall wlsunset`).catch(console.error);
+  private static get active() {
+    try {
+      return exec("systemctl --user is-active wlsunset.service") === "active";
+    } catch {
+      return false;
+    }
   }
 
-  startProcess(low: number, high: number) {
-    const location = Location.get_default().location;
-    const command = `wlsunset -l ${location.lat} -L ${location.lon} -t ${low} -T ${high}`;
-    bash(command).catch(console.error);
+  private syncServiceStatus() {
+    bind(this, "enabled").subscribe((enabled) => {
+      exec(`systemctl --user ${enabled ? "start" : "stop"} wlsunset.service`);
+    });
   }
 }
