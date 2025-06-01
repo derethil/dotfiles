2570351e68cdc0d0874e8296c6d79c0a6e7038ab
Jaren Glenn <jarenglenn@gmail.com>
2025-05-31 20:05:10 -0400

    feat(neovim): highlight glace-shell scss template literal

--- dots/nvim/lua/plugins/treesitter.lua
+++ dots/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "glsl", "graphql" },
+    ensure_installed = { "glsl", "graphql", "scss" },
+    injection = { enable = true },
   },
 }
---
+++ dots/nvim/queries/typescript/injections.scm
@@ -0,0 +1,6 @@
+(call_expression
+  function: (identifier) @tag
+  arguments: (template_string) @scss
+  (#eq? @tag "scss")
+  (#set! injection.language "scss"))
+
